86fdabf148566ca96f67c005b9c50cdd2b988ea6
Sincal200 <sincaljulio200@gmail.com>
2022-02-22 21:51:11 -0600

    Subo todos los archivos de la tarea C# - Básico

---
+++ Mi_primer_programa/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Fundamentos
+{
+    class Mi_primer_programa
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hazlo facil, haz Click");
+        }
+    }
+}
